98ebaaf39cd9c3e8659c6b306d5c718e76feccee
Sebastian Castillo <scastillo27@gmail.com>
2021-07-18 19:23:34 -0600

    CORS y Express- primer commit

---
+++ database/config.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+const dbConnection = async() => {
+
+    try{
+        await mongoose.connect(process.env.DB_CNN, {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+
+        console.log('DB Online');
+
+    }catch(error) {
+        console.log(error);
+        throw new Error('Error a la hora de iniciar la DB')
+    }
+
+
+}
+
+module.exports = {
+    dbConnection
+}
